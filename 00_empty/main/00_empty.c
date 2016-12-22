3db1e85737f6fd01f7886465818034603a4dab73
dentellaluca <luca.dentella@consulente.ingdirect.it>
2016-12-22 11:51:25 +0100

    Empty example added

---
+++ 00_empty/main/00_empty.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+
+void loop_task(void *pvParameter)
+{
+    while(1) {
+        
+		printf("Hello World!\n");
+        vTaskDelay(5000 / portTICK_RATE_MS);
+    }
+}
+
+void app_main()
+{
+	
+    xTaskCreate(&loop_task, "loop_task", 512, NULL, 5, NULL);
+}
