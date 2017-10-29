80a96cd0836bf6c7f37c6294aa49c14762c0b7ec
lucadentella <luca@dentella.it>
2017-10-29 20:56:49 +0100

    Added tutorial for u8g2 library

---
+++ 18_u8g2/components/u8g2/sys/sdl/hello_world_8x8/main.c
@@ -0,0 +1,23 @@
+#include "u8x8.h"
+
+u8x8_t u8x8;
+
+
+int main(void)
+{
+  u8x8_Setup_SDL_128x64(&u8x8);
+  u8x8_InitDisplay(&u8x8);  
+
+  u8x8_SetFont(&u8x8, u8x8_font_amstrad_cpc_extended_f);
+  
+  u8x8_DrawString(&u8x8, 0, 0, "Hello World!");
+
+  u8x8_Draw2x2String(&u8x8, 0, 3, "Hello World!");
+
+
+  while( u8g_sdl_get_key() < 0 )
+    ;
+  
+  return 0;
+}
+
---
+++ 18_u8g2/main/main.c
@@ -0,0 +1,67 @@
+#include <driver/gpio.h>
+#include <driver/spi_master.h>
+#include <esp_log.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "u8g2_esp32_hal.h"
+#include "icons.h"
+
+#define PIN_SDA 5
+#define PIN_SCL 4
+
+
+void app_main() {
+
+	// initialize the u8g2 hal
+	u8g2_esp32_hal_t u8g2_esp32_hal = U8G2_ESP32_HAL_DEFAULT;
+	u8g2_esp32_hal.sda = PIN_SDA;
+	u8g2_esp32_hal.scl = PIN_SCL;
+	u8g2_esp32_hal_init(u8g2_esp32_hal);
+
+	// initialize the u8g2 library
+	u8g2_t u8g2;
+	u8g2_Setup_ssd1306_128x64_noname_f(
+		&u8g2,
+		U8G2_R0,
+		u8g2_esp32_msg_i2c_cb,
+		u8g2_esp32_msg_i2c_and_delay_cb);
+	
+	// set the display address
+	u8x8_SetI2CAddress(&u8g2.u8x8, 0x78);
+	
+	// initialize the display
+	u8g2_InitDisplay(&u8g2);
+	
+	// wake up the display
+	u8g2_SetPowerSave(&u8g2, 0);
+	
+	// loop
+	while(1) {
+		
+		// draw the hourglass animation, full-half-empty
+		u8g2_ClearBuffer(&u8g2);
+		u8g2_DrawXBM(&u8g2, 34, 2, 60, 60, hourglass_full);
+		u8g2_SendBuffer(&u8g2);
+		vTaskDelay(1000 / portTICK_RATE_MS);
+		
+		u8g2_ClearBuffer(&u8g2);
+		u8g2_DrawXBM(&u8g2, 34, 2, 60, 60, hourglass_half);
+		u8g2_SendBuffer(&u8g2);
+		vTaskDelay(1000 / portTICK_RATE_MS);
+
+		u8g2_ClearBuffer(&u8g2);
+		u8g2_DrawXBM(&u8g2, 34, 2, 60, 60, hourglass_empty);
+		u8g2_SendBuffer(&u8g2);
+		vTaskDelay(1000 / portTICK_RATE_MS);	
+		
+		// set font and write hello world
+		u8g2_ClearBuffer(&u8g2);
+		u8g2_SetFont(&u8g2, u8g2_font_timR14_tf);
+		u8g2_DrawStr(&u8g2, 2,17,"Hello World!");
+		u8g2_SendBuffer(&u8g2);
+		vTaskDelay(5000 / portTICK_RATE_MS);
+	}	
+}
